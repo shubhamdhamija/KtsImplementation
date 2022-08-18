be1e755706b38b0c66ed0f03e11462acc5357adb
GitHub Action <action@github.com>
2022-08-18 05:06:10 +0000

    Template cleanup

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -61,7 +61,7 @@ publishing {
                     version = "$version-SNAPSHOT"
                 }
                 description.set("A template for Kotlin Android projects")
-                url.set("https://github.com/cortinico/kotlin-android-template/")
+                url.set("https://github.com/shubhamdhamija/ktsimplementation/")
 
                 licenses {
                     license {
@@ -71,18 +71,18 @@ publishing {
                 }
                 developers {
                     developer {
-                        id.set("cortinico")
-                        name.set("Nicola Corti")
+                        id.set("shubhamdhamija")
+                        name.set("shubhamdhamija")
                     }
                 }
                 scm {
-                    connection.set("scm:git:git://github.com/cortinico/kotlin-android-template.git")
-                    developerConnection.set("scm:git:ssh://github.com/cortinico/kotlin-android-template.git")
-                    url.set("https://github.com/cortinico/kotlin-android-template/")
+                    connection.set("scm:git:git://github.com/shubhamdhamija/ktsimplementation.git")
+                    developerConnection.set("scm:git:ssh://github.com/shubhamdhamija/ktsimplementation.git")
+                    url.set("https://github.com/shubhamdhamija/ktsimplementation/")
                 }
                 issueManagement {
                     system.set("GitHub Issues")
-                    url.set("https://github.com/cortinico/kotlin-android-template/issues")
+                    url.set("https://github.com/shubhamdhamija/ktsimplementation/issues")
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("ktsimplementation")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
